bf3cbe3b54f72d8cfd255cc97df378b6d2532420
aki123a <aki0710abc@gmail.com>
2024-10-24 15:03:44 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end  
+
   private
   def item_params
     params.require(:item).permit(:image,:item_name,:explanation,:category_id,:condition_id,:shipping_fee_id,:shipping_area_id,:delivery_day_id,:price).merge(user_id: current_user.id)
