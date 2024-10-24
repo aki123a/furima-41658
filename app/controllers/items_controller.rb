0aac54f57dc56abe120fb8dc5e858a29a6ac7a08
aki123a <aki0710abc@gmail.com>
2024-10-24 15:26:03 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :edit, :update]
-    before_action :set_item, only: [:show,:edit,:update]
+    before_action :set_item, only: [:show,:edit,:update,:destroy]
   def index
     @items =Item.includes( :user).order(created_at: :desc)
   end
