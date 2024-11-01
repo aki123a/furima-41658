7d3a010ff869ee7554e7f8f062727c5e3f7834dd
aki123a <aki0710abc@gmail.com>
2024-11-01 13:07:57 +0900

    order_address model修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,7 +4,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :shipping_area_id, numericality: { other_than: 0}
     validates :city
     validates :address_banti
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
